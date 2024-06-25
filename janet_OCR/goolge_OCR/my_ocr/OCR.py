104bb0aae40c2ec880a76ff8253f9309d347da26
Lishin <99582713+Lishin1215@users.noreply.github.com>
2024-06-25 17:44:54 +0100

    upload janet_OCR folder

---
+++ janet_OCR/goolge_OCR/my_ocr/OCR.py
@@ -0,0 +1,38 @@
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+import io
+
+class OCRDetector:
+
+    def __init__(self):
+        # Instantiates a client
+        self.client = vision.ImageAnnotatorClient()
+
+    def run_quickstart(self, file_path: str) -> str:
+        """Provides a quick start example for Cloud Vision."""
+
+        # Instantiates a client
+        # client = vision.ImageAnnotatorClient()
+
+        # The path of the image file to annotate
+        # file_path = "assests/Referral_letter_example.jpg"
+
+        # Loads the image into memory
+        with io.open(file_path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.Image(content=content)
+
+        # Performs label detection on the image file
+        response = self.client.text_detection(image=image)
+        texts = response.text_annotations
+
+        return texts[0].description
+
+
+
+if __name__ == "__main__":
+    detector = OCRDetector()
+    text = detector.run_quickstart("assests/Referral_letter_example.jpg")
+    print(text)
---
+++ janet_OCR/goolge_OCR/my_ocr/__init__.py
@@ -0,0 +1,1 @@
+from .OCR import OCRDetector
