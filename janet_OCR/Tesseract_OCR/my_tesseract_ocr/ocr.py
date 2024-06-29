c415998413e2f0dc63b3451ee995c9630f2c0f88
Lishin <99582713+Lishin1215@users.noreply.github.com>
2024-06-29 18:01:13 +0100

    write tesserct_ocr function and add a new my_tesseract_folder

---
+++ janet_OCR/Tesseract_OCR/my_tesseract_ocr/ocr.py
@@ -0,0 +1,25 @@
+import os
+import ctypes
+
+import pytesseract
+from PIL import Image
+
+class TesseractOCR:
+
+    def __init__(self, executable_path):
+        self.executable_path = executable_path
+        pytesseract.pytesseract.tesseract_cmd = self.executable_path
+
+    def perform_ocr(self, image_path):
+        image = Image.open(image_path)
+
+        return pytesseract.image_to_string(image)
+
+
+
+if __name__ == "__main__":
+    ocr = TesseractOCR('/opt/homebrew/Cellar/tesseract/5.4.1/bin/tesseract')
+    result_string = ocr.perform_ocr("assests/Referral_letter_example.jpg")
+    print(result_string)
+
+
