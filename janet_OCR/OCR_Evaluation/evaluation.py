57207351e59a77019a92d17b10c45ab376ab4917
Lishin <99582713+Lishin1215@users.noreply.github.com>
2024-07-04 15:17:17 +0100

    add another evaluate function(whole_dataset)

--- janet_OCR/OCR_Evaluation/evaluation.py
+++ janet_OCR/OCR_Evaluation/evaluation.py
@@ -3,7 +3,8 @@ import json
 from my_ocr import OCRDetector
 from my_aws_ocr import AWSTextDetector
 from my_tesseract_ocr import TesseractOCR
-from jiwer import wer
+from jiwer import wer, cer
+from tqdm import tqdm
 
 class Eval:
 
@@ -13,22 +14,22 @@ class Eval:
         self.tesseract_detector = TesseractOCR("/opt/homebrew/Cellar/tesseract/5.4.1/bin/tesseract")
 
 
-    def get_dict_from_dataset(self, directory_path: str) -> dict:
-        "read all dataset and save it to a dict (ex.{filename: result_string}"
-        results = {}
+    # def get_dict_from_dataset(self, directory_path: str) -> dict:
+    #     "read all dataset and save it to a dict (ex.{filename: result_string}"
+    #     results = {}
+    #
+    #     # Get all files in the directory
+    #     for filename in os.listdir(directory_path):
+    #         if filename.endswith(".jpg") or filename.endswith(".png"):
+    #             # Construct the full file path
+    #             file_path = os.path.join(directory_path, filename)
+    #             text = self.run_quickstart(file_path)
+    #             results[filename] = text
+    #
+    #     return results
 
-        # Get all files in the directory
-        for filename in os.listdir(directory_path):
-            if filename.endswith(".jpg") or filename.endswith(".png"):
-                # Construct the full file path
-                file_path = os.path.join(directory_path, filename)
-                text = self.run_quickstart(file_path)
-                results[filename] = text
 
-        return results
-
-
-    def getSortedText(self, annotation_file, tolerance = 5) -> str:
+    def getSortedText(self, annotation_file, tolerance = 10) -> str:
 
         data = json.load(annotation_file)
         forms = data.get('form', [])
@@ -73,23 +74,25 @@ class Eval:
 
 
 
-    def calculate_score(self, detected_text: str, correct_text: str) -> float:
+    def calculate_error(self, detected_text: str, correct_text: str) -> float:
         # calculate logic (error = wer(reference, hypothesis))
-        error = wer(correct_text, detected_text)
+        wer_error = wer(correct_text, detected_text)
+        cer_error = cer(correct_text, detected_text)
 
-        return error
+        return wer_error, cer_error
 
 
 
-    def calculate_score_from_dataset(self, detector_type: str, base_directory_path: str) -> float:
+    def calculate_error_from_dataset_file(self, detector_type: str, base_directory_path: str) -> float:
 
-        total_error = 0.0
+        wer_total_error = 0.0
+        cer_total_error = 0.0
         num_images = 0
 
         image_directory_path = os.path.join(base_directory_path, 'images')
         annotation_directory_path = os.path.join(base_directory_path, 'annotations')
 
-        for filename in os.listdir(image_directory_path):
+        for filename in tqdm(os.listdir(image_directory_path)):
             if filename.endswith(".jpg") or filename.endswith(".png"):
                 image_file_path = os.path.join(image_directory_path, filename)
 
@@ -120,26 +123,101 @@ class Eval:
 
                 if groundtruth is not None:
                     # compare origin text and ocr text --> get a score
-                    error = self.calculate_score(detect_text, groundtruth)
-                    total_error += error
+                    wer_error, cer_error = self.calculate_error(detect_text, groundtruth)
+                    wer_total_error += wer_error
+                    cer_total_error += cer_error
+
                     num_images += 1
 
         # calculate the average score (error越低越準確）
-        average_error: float = total_error / num_images if num_images > 0 else 0.0
+        wer_average_error: float = wer_total_error / num_images if num_images > 0 else 0.0
+        cer_average_error: float = cer_total_error / num_images if num_images > 0 else 0.0
 
-        return average_error
+        return wer_average_error, cer_average_error
+
+
+    def calculate_error_from_dataset_whole(self, detector_type: str, base_directory_path: str) -> float:
+
+        wer_total_error = 0.0
+        cer_total_error = 0.0
+        num_images = 0
+        all_detected_text = ""
+        all_groundtruth = ""
+
+        image_directory_path = os.path.join(base_directory_path, 'images')
+        annotation_directory_path = os.path.join(base_directory_path, 'annotations')
+
+        for filename in tqdm(os.listdir(image_directory_path)):
+            if filename.endswith(".jpg") or filename.endswith(".png"):
+                image_file_path = os.path.join(image_directory_path, filename)
+
+                # remove file extension(ex. .png/.json) from filename
+                filename_without_extension = os.path.splitext(filename)[0]
+
+                json_file_path = os.path.join(annotation_directory_path, filename_without_extension + '.json')
+
+                if os.path.isfile(json_file_path):
+                    # load "correct answer" from json
+                    with open(json_file_path, 'r') as f:
+
+                        groundtruth = self.getSortedText(f)
+                        all_groundtruth += groundtruth + " "
+
+
+
+                if detector_type == "google" :
+                    detect_text = self.google_detector.run_quickstart(image_file_path)
+                elif detector_type == "aws":
+                    detect_text = self.aws_detector.detect_file_text(document_file_name=image_file_path)
+                else:
+                    detect_text = self.tesseract_detector.perform_ocr(image_file_path)
+
+                all_detected_text += detect_text + " "
+
+        # print(f"Detected text: {all_detected_text}")
+        # print(f"Correct text: {all_groundtruth}")
+
+
+        if groundtruth is not None:
+            # compare origin text and ocr text --> get a score
+            wer_total_error, cer_total_error = self.calculate_error(all_detected_text, all_groundtruth)
+
+
+        return wer_total_error, cer_total_error
+
 
 
 if __name__ == "__main__":
     eval = Eval()
 
-    google_average_error = eval.calculate_score_from_dataset("google", "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/training_data")
-    print(google_average_error)
+    # [per file]
+    # print("per file: ")
+    # google_average_wer_error, google_average_cer_error = eval.calculate_error_from_dataset_file("google", "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/testing_data")
+    # print(google_average_wer_error, google_average_cer_error)
+    #
+    # aws_average_wer_error, aws_average_cer_error = eval.calculate_error_from_dataset_file("aws",
+    #                                   "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/testing_data")
+    # print(aws_average_wer_error, aws_average_cer_error)
+    #
+    # tesseract_average_wer_error, tesseract_average_cer_error = eval.calculate_error_from_dataset_file("tesseract",
+    #                                   "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/testing_data")
+    # print(tesseract_average_wer_error, tesseract_average_cer_error)
 
-    aws_average_error = eval.calculate_score_from_dataset("aws",
-                                      "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/training_data")
-    print(aws_average_error)
-    
-    tesseract_average_error = eval.calculate_score_from_dataset("tesseract",
-                                      "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/training_data")
-    print(tesseract_average_error)
+    # [whole]
+    print("============================================================================")
+    print("whole: ")
+    google_total_wer_error, google_total_cer_error = eval.calculate_error_from_dataset_whole("google",
+                                                                                                "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/testing_data")
+    print(google_total_wer_error, google_total_cer_error)
+
+    aws_total_wer_error, aws_total_cer_error = eval.calculate_error_from_dataset_whole("aws",
+                                                                                          "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/testing_data")
+    print(aws_total_wer_error, aws_total_cer_error)
+
+    tesseract_total_wer_error, tesseract_total_cer_error = eval.calculate_error_from_dataset_whole("tesseract",
+                                                                                                      "/Users/lishin/Desktop/Bristol/Summer Project/UOB_2024_LLM_EYE_HOSPTITAL/janet_OCR/dataset/testing_data")
+    print(tesseract_total_wer_error, tesseract_total_cer_error)
+
+
+
+
